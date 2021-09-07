8caf16daa0f53e84a8c55fefa5eb401b41d4c0ae
Patrick Zimbrod <76100656+pzimbrod@users.noreply.github.com>
2021-09-07 15:33:29 +0200

    prepare tests

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -2,22 +2,25 @@ from prosi3d.sensors.acousticair import Accousticair
 from prosi3d.sensors.acousticplatform import Accousticplatform
 from prosi3d.sensors.recoater import Recoater
 
-hdf_name = 'C:\\Users\\zimbropa\\Downloads\\ch4raw_00581.h5'
+hdf_name = 'data/ch4raw_00593.h5'
 
 acc = Accousticair()
 acc.get_data(hdf_name)
 acc.process()
-acc.plot_test()
+## No plots for CI
+#acc.plot_test()
 acc.write()
 
 acc_p = Accousticplatform()
 acc_p.get_data(hdf_name)
 acc_p.process()
-acc_p.plot_test()
+## No plots for CI
+#acc_p.plot_test()
 acc_p.write()
 
 acc_r = Recoater()
 acc_r.get_data(hdf_name)
 acc_r.process()
-acc_r.plot_test()
+# No plots for CI
+#acc_r.plot_test()
 acc_r.write()
--- tests/test_analysis_loop.py
+++ tests/test_analysis_loop.py
@@ -2,7 +2,7 @@ from prosi3d.sensors.acousticair import Accousticair
 from prosi3d.sensors.acousticplatform import Accousticplatform
 from prosi3d.sensors.recoater import Recoater
 
-hdf_name = 'C:\\Users\\zimbropa\\Downloads\\ch4raw_00581.h5'
+hdf_name = 'data\ch4raw_00593.h5'
 
 acc = Accousticair()
 acc_p = Accousticplatform()
@@ -13,5 +13,6 @@ sensors = [acc,acc_p,acc_r]
 for sensor in sensors:
     sensor.get_data(hdf_name)
     sensor.process()
-    sensor.plot_test()
+    # No plots for CI
+    #sensor.plot_test()
     sensor.write()
