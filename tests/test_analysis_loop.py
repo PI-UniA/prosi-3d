ce598d688ca010fb9ec6221b13798aca5bf27c62
zimbropa <patrick.zimbrod@informatik.uni-augsburg.de>
2021-09-08 08:53:14 +0200

    🧑‍🔧 correct file path

--- tests/test_analysis_loop.py
+++ tests/test_analysis_loop.py
@@ -2,7 +2,7 @@ from prosi3d.sensors.acousticair import Accousticair
 from prosi3d.sensors.acousticplatform import Accousticplatform
 from prosi3d.sensors.recoater import Recoater
 
-hdf_name = 'data\ch4raw_00593.h5'
+hdf_name = 'data/ch4raw_00593.h5'
 
 acc = Accousticair()
 acc_p = Accousticplatform()
