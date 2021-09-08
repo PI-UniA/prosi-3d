afaae834a75e3673698106426ee68957c3c20322
zimbropa <patrick.zimbrod@informatik.uni-augsburg.de>
2021-09-08 09:31:23 +0200

    🧪 refactor tests to conform with pytest

--- tests/test_analysis_loop.py
+++ tests/test_analysis_loop.py
@@ -1,18 +1,21 @@
+import pytest
+
 from prosi3d.sensors.acousticair import Accousticair
 from prosi3d.sensors.acousticplatform import Accousticplatform
 from prosi3d.sensors.recoater import Recoater
 
-hdf_name = 'data/ch4raw_00593.h5'
+def test_analysis_loop():
+    hdf_name = 'data/ch4raw_00593.h5'
 
-acc = Accousticair()
-acc_p = Accousticplatform()
-acc_r = Recoater()
+    acc = Accousticair()
+    acc_p = Accousticplatform()
+    acc_r = Recoater()
 
-sensors = [acc,acc_p,acc_r]
+    sensors = [acc,acc_p,acc_r]
 
-for sensor in sensors:
-    sensor.get_data(hdf_name)
-    sensor.process()
-    # No plots for CI
-    #sensor.plot_test()
-    sensor.write()
+    for sensor in sensors:
+        sensor.get_data(hdf_name)
+        sensor.process()
+        # No plots for CI
+        #sensor.plot_test()
+        sensor.write()
