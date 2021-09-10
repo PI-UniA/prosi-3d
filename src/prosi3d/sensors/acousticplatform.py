d30c7f99faec4dc953328d77b44e46d7a8cfcc82
stolzean <annalena.stolze@student.uni-augsburg.de>
2021-09-10 15:57:26 +0200

    Änderungen in den anderen Klassen

--- src/prosi3d/sensors/acousticplatform.py
+++ src/prosi3d/sensors/acousticplatform.py
@@ -1,3 +1,5 @@
+""" Subclass from Abstract Base Class featureExtractor that outputs features of the raw data that are required for machine learning models """
+
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py as h5
@@ -6,33 +8,24 @@ import sys
 import math
 
 from prosi3d.meta.featureExtractor import FeatureExtractor
-from prosi3d.sensors.methodsCollection import MethodsCollections
+from prosi3d.sensors._methodsCollection import _MethodsCollections
 
-""" Subclass from Abstract Base Class featureExtractor that outputs features of the raw data that are required for machine learning models """
 class Accousticplatform(FeatureExtractor):
 
-    """ Accousticplatform is subclass of FeatureExtractor providing the Methods get_data, process, and write.
-        
+    """ 
         Attribute: 
-            xt:
-            yt:
-            xf: 
-            yf:
-            peaks_x:
-            peaks_y:
+            peaks_x (numpy.array): frequency of the peaks
+            peaks_y (numpy.array): spectral power density of the peaks
     """
 
 
     
     def get_data(self, hdf):
-        """ Extract the measurements of the accousticplatform from the hdf5 file
+        """ Extract the measurements of the accousticplatform sensor from the hdf5 file.
         
         Args:
-            hdf: path of the hdf5 file.
+            hdf (String): path of the hdf5 file.
 
-        Returns:
-            Assign the measurements to the attribute yt and the time values to the attribute xt
-        
         Raises:
             IOError: File can not found.
         """
@@ -42,7 +35,7 @@ class Accousticplatform(FeatureExtractor):
         
 
     def process(self):
-        """ Convert the signal of the time domain to a representation in the frequency domain using the rFFT. Identify particularly conspicuous peaks in the frequency domain. """
+        """ Convert the signal of the time domain to the representation in the frequency domain using the rFFT. Identify particularly conspicuous peaks in the frequency domain. """
         
         # method to replace nan values
         self._replace_nan()
@@ -61,38 +54,36 @@ class Accousticplatform(FeatureExtractor):
 
     
     def write(self):
-        """ Print the x-values and the y-values of the peaks which are saved in to numpy arrays.
-        
-        """
+        """ Print the x-values and the y-values of the peaks (frequency domain) which are saved in numpy arrays. """
 
         print("x-Werte Peaks: ", self.peaks_x)
         print("y-Werte Peaks: ", self.peaks_y)
 
 
-    """ Abstract method from freatureExtractor to create the time domain """
+    # Abstract method from freatureExtractor to create the time domain
     def _read_measurements(self, hdf):
         
         # Measurements of the accousticplatform in column 3
         sensorwert = 2 
-        self.xt, self.yt = MethodsCollections._read_measurements_C(hdf, sensorwert)
+        self.xt, self.yt = _MethodsCollections._read_measurements_C(hdf, sensorwert)
 
 
-    """ Abstract method from featureExtractor to replace nan values with the mean of the neighboring values """
+    # Abstract method from featureExtractor to replace nan values with the mean of the neighboring values
     def _replace_nan(self): 
-        self.yt = MethodsCollections._replace_nan_C(self.yt)
+        self.yt = _MethodsCollections._replace_nan_C(self.yt)
         
 
-    """ Abstract method from featureExtractor to shift the x-axis to the mean """
+    # Abstract method from featureExtractor to shift the x-axis to the mean
     def _move_to_mean(self):
-        self.yt = MethodsCollections._move_to_mean(self.yt)
+        self.yt = _MethodsCollections._move_to_mean(self.yt)
 
 
-    """ Abstract method from featureExtractor to create the rFFT """
+    # Abstract method from featureExtractor to create the rFFT """
     def _create_FFT(self):
-        self.xf, self.yf = MethodsCollections._create_FFT_C(self.yt)
+        self.xf, self.yf = _MethodsCollections._create_FFT_C(self.yt)
         
 
-    """ Abstract method from featureExtractor to find the peaks """
+    # Abstract method from featureExtractor to find the peaks
     def _find_peaks_values(self):
         
         try:
@@ -115,9 +106,8 @@ class Accousticplatform(FeatureExtractor):
 
     # Nur derzeitig zum Testen enthalten (kann später entfernt werden)
     def plot_test(self):
-        """ Plot the diagramms with the identified peaks in order to check the methods 
+        """ Plot the diagramms of the time domain and freqency domain with the identified peaks to check the result """
         
-        """
         try:
             fig, ax = plt.subplots(2)
             
@@ -132,7 +122,7 @@ class Accousticplatform(FeatureExtractor):
             ax[1].scatter(self.xf, self.yf, s=2)
             ax[1].set_title(f'Frequenzbereich')
             ax[1].set_xlabel('Frequenz in [Hz]')
-            ax[1].set_ylabel('Spektale Leistungsdichte')
+            ax[1].set_ylabel('Spektrale Leistungsdichte')
             plt.ylim(-0.0000005, 0.000005) ###Achtung: Wurde angepasst
 
             # plot peaks
--- src/prosi3d/sensors/recoater.py
+++ src/prosi3d/sensors/recoater.py
@@ -1,3 +1,5 @@
+""" Subclass from Abstract Base Class featureExtractor that outputs features of the raw data that are required for machine learning models """
+
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py as h5
@@ -6,78 +8,89 @@ from scipy.signal import find_peaks
 import math
 
 from prosi3d.meta.featureExtractor import FeatureExtractor
-from prosi3d.sensors.methodsCollection import MethodsCollections
+from prosi3d.sensors._methodsCollection import _MethodsCollections
 
-""" Subclass from Abstract Base Class featureExtractor that outputs features of the raw data that are required for machine learning models """
 class Recoater(FeatureExtractor):
 
+    """ 
+        Attribute: 
+            peaks_x (numpy.array): frequency of the peaks
+            peaks_y (numpy.array): spectral power density of the peaks
     """
-    xxxxx
-    """
-
-    """ Abstract method from preprocessor to extract the data from the hdf5 """
+    
     def get_data(self, hdf):
+        """ Extract the measurements of the recoater sensor from the hdf5 file. 
+        
+        Args:
+            hdf (String): path of the hdf5 file.
+
+        Raises:
+            IOError: File can not found.
+        """
        
-        """ method to read teh hdf5 file """
+        # method to read teh hdf5 file
         self._read_measurements(hdf)
         
 
-    """ Abstract method from preprocessor to process the data (FFT, find peaks) """
+    
     def process(self):
+        """ Convert the signal of the time domain to the representation in the frequency domain using the rFFT. Identify particularly conspicuous peaks in the frequency domain. """
         
-        """ method to replace nan values """
+        # method to replace nan values 
         self._replace_nan()
         
-        """ method to shift the x-axis to the mean """
+        # method to shift the x-axis to the mean
         self._move_to_mean()
 
-        """ method to create the frequency datas (freqence, power spectral density) """
+        # method to create the frequency datas (freqence, power spectral density) 
         self._create_FFT()
 
-        """ method to find peaks """
+        # method to find peaks 
         peaks = self._find_peaks_values()
         self.peaks_x = np.array([self.xf[peaks[0]]])
         self.peaks_y = np.array([self.yf[peaks[0]]])
 
 
-    """ Abstract method from preprocessor to print the peaks """
     def write(self):
-        ###Aktuell: Numpy array mit x-Werten von Peak und Numpy Array mit y-Werten von Peak
+        """ Print the x-values and the y-values of the peaks (frequency domain) which are saved in numpy arrays. """
+
         print("x-Werte Peaks: ", self.peaks_x)
         print("y-Werte Peaks: ", self.peaks_y)
 
 
-    """ Abstract method from freatureExtractor to create the time domain """
+    # Abstract method from freatureExtractor to create the time domain
     def _read_measurements(self, hdf):
-        sensorwert = 3 ###Sensorwert: Annahme 3!
-        self.xt, self.yt = MethodsCollections._read_measurements_C(hdf, sensorwert)
+        
+        # Measurements of the recoater in column 4
+        sensorwert = 3
+        self.xt, self.yt = _MethodsCollections._read_measurements_C(hdf, sensorwert)
 
 
-    """ Abstract method from featureExtractor to replace nan values with the mean of the neighboring values """
+    # Abstract method from featureExtractor to replace nan values with the mean of the neighboring values
     def _replace_nan(self): 
-        self.yt = MethodsCollections._replace_nan_C(self.yt)
+        self.yt = _MethodsCollections._replace_nan_C(self.yt)
         
 
-    """ Abstract method from featureExtractor to shift the x-axis to the mean """
+    # Abstract method from featureExtractor to shift the x-axis to the mean
     def _move_to_mean(self):
-        self.yt = MethodsCollections._move_to_mean(self.yt)
+        self.yt = _MethodsCollections._move_to_mean(self.yt)
 
 
-    """ Abstract method from featureExtractor to create the rFFT """
+    # Abstract method from featureExtractor to create the rFFT
     def _create_FFT(self):
-        self.xf, self.yf = MethodsCollections._create_FFT_C(self.yt)
+        self.xf, self.yf = _MethodsCollections._create_FFT_C(self.yt)
         
 
-    """ Abstract method from featureExtractor to find the peaks """
+    # Abstract method from featureExtractor to find the peaks
     def _find_peaks_values(self):
         
         try:
-            ###Parameter müssen noch gewählt werden, siehe https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html
+            # TODO Parameter müssen noch gewählt werden, siehe https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html
             height = 1*1e-6
             distance = None
             prominence = None
 
-            """return value: index of the peaks"""
+            # return value: index of the peaks
             peaks = find_peaks(self.yf, height, distance, prominence)
             
             return peaks
@@ -87,26 +100,27 @@ class Recoater(FeatureExtractor):
 
 
 
-    """ method to plot the diagramms and the peaks """
     ###Nur derzeitig zum Testen enthalten (kann später entfernt werden)
     def plot_test(self):
+        """ Plot the diagramms of the time domain and freqency domain with the identified peaks to check the result """
+        
         try:
             fig, ax = plt.subplots(2)
             
-            """ plot time Domain """
+            # plot time domain
             ax[0].plot(self.xt, self.yt, linewidth=0.1)
             ax[0].set_title('Zeitbereich')
             ax[0].set_xlabel('Zeit in [ms]')
             ax[0].set_ylabel('Sensormesswert')
 
-            """plot frequency domain"""
+            # plot frequency domain
             ax[1].scatter(self.xf, self.yf, s=2)
             ax[1].set_title(f'Frequenzbereich')
             ax[1].set_xlabel('Frequenz in [Hz]')
-            ax[1].set_ylabel('Spektale Leistungsdichte')
+            ax[1].set_ylabel('Spektrale Leistungsdichte')
             plt.ylim(-0.0000005, 0.000005) ###Achtung: Wurde angepasst
 
-            """plot peaks"""
+            # plot peaks
             ax[1].scatter(self.peaks_x, self.peaks_y, marker="x")
 
             fig.suptitle("Recoater")
