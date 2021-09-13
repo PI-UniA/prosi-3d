18f137a864b3eebaccf276c39a4547c32ce50657
stolzean <annalena.stolze@student.uni-augsburg.de>
2021-09-13 15:43:01 +0200

    Klassen grob aufgebaut

--- src/prosi3d/sensors/nircamera.py
+++ src/prosi3d/sensors/nircamera.py
@@ -1,1 +1,30 @@
-from ..meta import featureExtractor
+""" Subclass from Abstract Base Class featureExtractor that outputs features of the raw data that are required for machine learning models """
+
+import numpy as np
+
+from prosi3d.meta.featureExtractor import FeatureExtractor
+
+class Nircamera (FeatureExtractor):
+
+    """ 
+    Attribute: 
+        xxx: xxx.
+        xxx: xxx.
+    """
+
+
+    def get_data(self):
+        """ Beschreibung """
+        pass
+
+    def process(self):
+        """ Beschreibung """
+        pass
+
+    def write(self):
+        """ Beschreibung """
+        pass
+
+    def get_feature(self):
+        """ Beschreibung """
+        pass
